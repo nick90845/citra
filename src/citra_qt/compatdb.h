8896c3f7735508a2f11aaddbac66aab6f8add4cd
citrabot <citra@citra-emu.org>
2018-01-19 15:09:44 +0000

    Merge PR 3325

---
+++ src/citra_qt/compatdb.h
@@ -0,0 +1,29 @@
+// Copyright 2017 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include <memory>
+#include <QWizard>
+
+namespace Ui {
+class CompatDB;
+}
+
+class CompatDB : public QWizard {
+    Q_OBJECT
+
+public:
+    explicit CompatDB(QWidget* parent = nullptr);
+    ~CompatDB();
+
+private:
+    std::unique_ptr<Ui::CompatDB> ui;
+
+private slots:
+    void Submit();
+    void EnableNext();
+};
+
+enum CompatDBPage { IntroPage = 0, SelectionPage = 1, FinalPage = 2 };
