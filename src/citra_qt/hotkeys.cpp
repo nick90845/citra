6dc26ccaa74058ceab266ce8428e5317ab34ab8b
citrabot <citra@citra-emu.org>
2018-08-20 14:19:17 +0000

    Merge PR 3786

--- src/citra_qt/hotkeys.cpp
+++ src/citra_qt/hotkeys.cpp
@@ -2,32 +2,20 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
-#include <map>
 #include <QKeySequence>
 #include <QShortcut>
 #include <QtGlobal>
 #include "citra_qt/hotkeys.h"
 #include "citra_qt/ui_settings.h"
 
-struct Hotkey {
-    Hotkey() : shortcut(nullptr), context(Qt::WindowShortcut) {}
-
-    QKeySequence keyseq;
-    QShortcut* shortcut;
-    Qt::ShortcutContext context;
-};
-
-typedef std::map<QString, Hotkey> HotkeyMap;
-typedef std::map<QString, HotkeyMap> HotkeyGroupMap;
-
-HotkeyGroupMap hotkey_groups;
+HotkeyGroupMap hotkey_groups = {};
 
 void SaveHotkeys() {
     UISettings::values.shortcuts.clear();
     for (auto group : hotkey_groups) {
         for (auto hotkey : group.second) {
             UISettings::values.shortcuts.emplace_back(
-                UISettings::Shortcut(group.first + "/" + hotkey.first,
+                UISettings::Shortcut(hotkey.first, group.first,
                                      UISettings::ContextualShortcut(hotkey.second.keyseq.toString(),
                                                                     hotkey.second.context)));
         }
@@ -38,25 +26,13 @@ void LoadHotkeys() {
     // Make sure NOT to use a reference here because it would become invalid once we call
     // beginGroup()
     for (auto shortcut : UISettings::values.shortcuts) {
-        QStringList cat = shortcut.first.split("/");
-        Q_ASSERT(cat.size() >= 2);
-
-        // RegisterHotkey assigns default keybindings, so use old values as default parameters
-        Hotkey& hk = hotkey_groups[cat[0]][cat[1]];
-        if (!shortcut.second.first.isEmpty()) {
-            hk.keyseq = QKeySequence::fromString(shortcut.second.first);
-            hk.context = (Qt::ShortcutContext)shortcut.second.second;
+        Hotkey& hk = hotkey_groups[shortcut.group][shortcut.name];
+        if (!shortcut.shortcut.first.isEmpty()) {
+            hk.keyseq = QKeySequence::fromString(shortcut.shortcut.first);
+            hk.context = (Qt::ShortcutContext)shortcut.shortcut.second;
         }
         if (hk.shortcut)
             hk.shortcut->setKey(hk.keyseq);
-    }
-}
-
-void RegisterHotkey(const QString& group, const QString& action, const QKeySequence& default_keyseq,
-                    Qt::ShortcutContext default_context) {
-    if (hotkey_groups[group].find(action) == hotkey_groups[group].end()) {
-        hotkey_groups[group][action].keyseq = default_keyseq;
-        hotkey_groups[group][action].context = default_context;
     }
 }
 
@@ -69,26 +45,12 @@ QShortcut* GetHotkey(const QString& group, const QString& action, QWidget* widge
     return hk.shortcut;
 }
 
-GHotkeysDialog::GHotkeysDialog(QWidget* parent) : QWidget(parent) {
-    ui.setupUi(this);
-
-    for (auto group : hotkey_groups) {
-        QTreeWidgetItem* toplevel_item = new QTreeWidgetItem(QStringList(group.first));
-        for (auto hotkey : group.second) {
-            QStringList columns;
-            columns << hotkey.first << hotkey.second.keyseq.toString();
-            QTreeWidgetItem* item = new QTreeWidgetItem(columns);
-            toplevel_item->addChild(item);
-        }
-        ui.treeWidget->addTopLevelItem(toplevel_item);
-    }
-    // TODO: Make context configurable as well (hiding the column for now)
-    ui.treeWidget->setColumnCount(2);
-
-    ui.treeWidget->resizeColumnToContents(0);
-    ui.treeWidget->resizeColumnToContents(1);
+QKeySequence GetKeySequence(const QString& group, const QString& action) {
+    Hotkey& hk = hotkey_groups[group][action];
+    return hk.keyseq;
 }
 
-void GHotkeysDialog::retranslateUi() {
-    ui.retranslateUi(this);
+Qt::ShortcutContext GetShortcutContext(const QString& group, const QString& action) {
+    Hotkey& hk = hotkey_groups[group][action];
+    return hk.context;
 }
