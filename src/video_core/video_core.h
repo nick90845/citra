77c6b4839613061d717400375daf05c605b0c151
citrabot <citra@citra-emu.org>
2018-08-24 17:31:27 +0000

    Merge PR 3970

--- src/video_core/video_core.h
+++ src/video_core/video_core.h
@@ -6,6 +6,7 @@
 
 #include <atomic>
 #include <memory>
+#include "core/core.h"
 
 class EmuWindow;
 class RendererBase;
@@ -32,7 +33,7 @@ extern std::atomic<bool> g_use_format_reinterpret_hack;
 void Start();
 
 /// Initialize the video core
-bool Init(EmuWindow* emu_window);
+Core::System::ResultStatus Init(EmuWindow* emu_window);
 
 /// Shutdown the video core
 void Shutdown();
