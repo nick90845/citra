b86b755e164e842ba5f081c77a9f92e19db164e1
citrabot <citra@citra-emu.org>
2018-09-15 06:31:00 +0000

    Merge PR 3959

--- src/citra_qt/main.cpp
+++ src/citra_qt/main.cpp
@@ -1401,7 +1401,6 @@ void GMainWindow::UpdateStatusBar() {
 }
 
 void GMainWindow::OnCoreError(Core::System::ResultStatus result, std::string details) {
-    QMessageBox::StandardButton answer;
     QString status_message;
 
     QString title, message;
@@ -1436,9 +1435,11 @@ void GMainWindow::OnCoreError(Core::System::ResultStatus result, std::string det
     message_box.setIcon(QMessageBox::Icon::Critical);
     QPushButton* continue_button = message_box.addButton(tr("Continue"), QMessageBox::RejectRole);
     QPushButton* abort_button = message_box.addButton(tr("Abort"), QMessageBox::AcceptRole);
-    message_box.exec();
+    if (result != Core::System::ResultStatus::ShutdownRequested)
+        message_box.exec();
 
-    if (message_box.clickedButton() == abort_button) {
+    if (result == Core::System::ResultStatus::ShutdownRequested ||
+        message_box.clickedButton() == abort_button) {
         if (emu_thread) {
             ShutdownGame();
         }
