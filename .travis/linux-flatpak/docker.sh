69498dab51e15ba21251134c8247ae2bbdcc3f62
citrabot <citra@citra-emu.org>
2018-10-28 16:13:09 +0000

    Merge PR 4383

---
+++ .travis/linux-flatpak/docker.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -ex
+
+# Converts "citra-emu/citra-nightly" to "citra-nightly"
+REPO_NAME=$(echo $TRAVIS_REPO_SLUG | cut -d'/' -f 2)
+CITRA_SRC_DIR="/citra"
+BUILD_DIR="$CITRA_SRC_DIR/build"
+REPO_DIR="$CITRA_SRC_DIR/repo"
+STATE_DIR="$CITRA_SRC_DIR/.flatpak-builder"
+KEYS_ARCHIVE="/tmp/keys.tar"
+SSH_KEY="/tmp/ssh.key"
+GPG_KEY="/tmp/gpg.key"
+
+# Update the host packages
+apt-get install -y flatpak flatpak-builder ca-certificates git sshfs curl fuse dnsutils gnupg2
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub org.kde.Platform//5.11 org.kde.Sdk//5.11 org.freedesktop.Sdk.Extension.gcc7
+
+# Extract keys
+openssl aes-256-cbc -K $FLATPAK_ENC_K -iv $FLATPAK_ENC_IV -in "$CITRA_SRC_DIR/keys.tar.enc" -out "$KEYS_ARCHIVE" -d
+tar -C /tmp -xvf $KEYS_ARCHIVE
+
+# Configure SSH keys
+eval "$(ssh-agent -s)"
+chmod -R 600 "$HOME/.ssh"
+chown -R root "$HOME/.ssh"
+chmod 600 "$SSH_KEY"
+ssh-add "$SSH_KEY"
+echo "[$FLATPAK_SSH_HOSTNAME]:$FLATPAK_SSH_PORT,[$(dig +short $FLATPAK_SSH_HOSTNAME)]:$FLATPAK_SSH_PORT $FLATPAK_SSH_PUBLIC_KEY" > ~/.ssh/known_hosts
+
+# Configure GPG keys
+gpg2 --import "$GPG_KEY"
+
+# Download the Citra compatibility list
+# This is a temporary workaround for Flatpak builder, as it fails to download
+# the compatibility list on its own
+curl --url https://api.citra-emu.org/gamedb/ -o "/tmp/compatibility_list.json"
+
+# Mount our flatpak repository
+mkdir -p "$REPO_DIR"
+sshfs "$FLATPAK_SSH_USER@$FLATPAK_SSH_HOSTNAME:/upload/repo" "$REPO_DIR" -C -p "$FLATPAK_SSH_PORT" -o IdentityFile="$SSH_KEY"
+
+# Build the citra flatpak
+flatpak-builder -v --jobs=4 --ccache --force-clean --state-dir="$STATE_DIR" --gpg-sign="$FLATPAK_GPG_PUBLIC_KEY" --repo="$REPO_DIR" "$BUILD_DIR" "/tmp/org.citra.$REPO_NAME.json"
+flatpak build-update-repo "$REPO_DIR" -v --generate-static-deltas --gpg-sign="$FLATPAK_GPG_PUBLIC_KEY"
