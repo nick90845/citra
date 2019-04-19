9ed74cc952d6f8cceee5cc59dafe8f618c1ae55f
citrabot <citra@citra-emu.org>
2019-04-19 00:02:41 +0000

    Merge PR 4739

--- .travis/linux-flatpak/docker.sh
+++ .travis/linux-flatpak/docker.sh
@@ -6,14 +6,9 @@ CITRA_SRC_DIR="/citra"
 BUILD_DIR="$CITRA_SRC_DIR/build"
 REPO_DIR="$CITRA_SRC_DIR/repo"
 STATE_DIR="$CITRA_SRC_DIR/.flatpak-builder"
-KEYS_ARCHIVE="/tmp/keys.tar"
 SSH_DIR="/upload"
-SSH_KEY="/tmp/ssh.key"
-GPG_KEY="/tmp/gpg.key"
-
-# Extract keys
-openssl aes-256-cbc -K $FLATPAK_ENC_K -iv $FLATPAK_ENC_IV -in "$CITRA_SRC_DIR/keys.tar.enc" -out "$KEYS_ARCHIVE" -d
-tar -C /tmp -xvf $KEYS_ARCHIVE
+SSH_KEY="${AZP_TMP_DIR}/ssh.key"
+GPG_KEY="${AZP_TMP_DIR}/gpg.key"
 
 # Configure SSH keys
 eval "$(ssh-agent -s)"
@@ -31,5 +26,5 @@ mkdir -p "$REPO_DIR"
 sshfs "$FLATPAK_SSH_USER@$FLATPAK_SSH_HOSTNAME:$SSH_DIR" "$REPO_DIR" -C -p "$FLATPAK_SSH_PORT" -o IdentityFile="$SSH_KEY"
 
 # Build the citra flatpak
-flatpak-builder -v --jobs=4 --ccache --force-clean --state-dir="$STATE_DIR" --gpg-sign="$FLATPAK_GPG_PUBLIC_KEY" --repo="$REPO_DIR" "$BUILD_DIR" "/tmp/org.citra.$REPO_NAME.json"
-flatpak build-update-repo "$REPO_DIR" -v --generate-static-deltas --gpg-sign="$FLATPAK_GPG_PUBLIC_KEY"
+flatpak-builder -v --jobs=4 --ccache --force-clean --state-dir="$STATE_DIR" --gpg-sign="$FLATPAK_GPG_KEY_ID" --repo="$REPO_DIR" "$BUILD_DIR" "/tmp/org.citra.$REPO_NAME.json"
+flatpak build-update-repo "$REPO_DIR" -v --generate-static-deltas --gpg-sign="$FLATPAK_GPG_KEY_ID"
