4d4fcc19fa4aacb187daa3c986c850397d58db96
citrabot <citra@citra-emu.org>
2019-05-01 09:00:27 +0000

    Merge PR 4578

--- src/video_core/renderer_opengl/gl_shader_util.cpp
+++ src/video_core/renderer_opengl/gl_shader_util.cpp
@@ -107,6 +107,7 @@ GLuint LoadProgram(bool separable_program, const std::vector<GLuint>& shaders) {
     for (GLuint shader : shaders) {
         if (shader != 0) {
             glDetachShader(program_id, shader);
+            glDeleteShader(shader);
         }
     }
 
